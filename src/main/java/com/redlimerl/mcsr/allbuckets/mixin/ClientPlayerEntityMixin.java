c1daff51061c1ab7669c0417ca00c044616013dc
RedLime <redlimerl@gmail.com>
2022-03-04 15:17:51 +0900

    port 1.12.2

--- src/main/java/com/redlimerl/mcsr/allbuckets/mixin/ClientPlayerEntityMixin.java
+++ src/main/java/com/redlimerl/mcsr/allbuckets/mixin/ClientPlayerEntityMixin.java
@@ -8,7 +8,6 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -26,24 +25,20 @@ public abstract class ClientPlayerEntityMixin extends PlayerEntity {
         GOAL_ITEMS.add(Items.LAVA_BUCKET);
         GOAL_ITEMS.add(Items.MILK_BUCKET);
         GOAL_ITEMS.add(Items.WATER_BUCKET);
-        GOAL_ITEMS.add(Items.COD_BUCKET);
-        GOAL_ITEMS.add(Items.TROPICAL_FISH_BUCKET);
-        GOAL_ITEMS.add(Items.PUFFERFISH_BUCKET);
-        GOAL_ITEMS.add(Items.SALMON_BUCKET);
     }
 
-    public ClientPlayerEntityMixin(World world, BlockPos blockPos, GameProfile gameProfile) {
-        super(world, blockPos, gameProfile);
+    public ClientPlayerEntityMixin(World world, GameProfile profile) {
+        super(world, profile);
     }
 
     @Inject(method = "tick", at = @At("TAIL"))
     public void onTicking(CallbackInfo ci) {
         if (InGameTimer.getInstance().getCategory() == AllBuckets.ALL_BUCKETS_CATEGORY && InGameTimer.getInstance().isPlaying()) {
             ArrayList<Item> itemList = new ArrayList<>();
-            for (ItemStack itemStack : this.inventory.main) {
-                if (itemStack != null && itemStack.getItem() != null && !itemList.contains(itemStack.getItem())) itemList.add(itemStack.getItem().asItem());
+            for (ItemStack itemStack : this.inventory.field_15082) {
+                if (itemStack != null && itemStack.getItem() != null && !itemList.contains(itemStack.getItem())) itemList.add(itemStack.getItem());
             }
-            for (ItemStack itemStack : this.inventory.offHand) {
+            for (ItemStack itemStack : this.inventory.field_15084) {
                 if (itemStack != null && itemStack.getItem() != null && !itemList.contains(itemStack.getItem())) itemList.add(itemStack.getItem());
             }
 
